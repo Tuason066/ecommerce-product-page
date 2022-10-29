9dab04aea0efdd4da8ecf65d644b3b4f9de681eb
Jeffrey Tuason <jepoyduque06@gmail.com>
2022-10-29 18:19:17 +0800

    checkout button

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,43 +9,48 @@ function Cart() {
         Cart
       </h2>
       {cart.length > 0 ? (
-        cart.map(({ id, name, newPrice, quantity, image }) => {
-          return (
-            <article
-              key={id}
-              className='grid grid-cols-[auto_1fr_auto] p-4 lg:p-6 gap-x-4'
-            >
-              <div className='col-start-1 col-end-2 row-start-1 row-end-3 w-12'>
-                <img className='rounded-md' src={image} alt={name} />
-              </div>
-              <h3 className='col-start-2 col-end-3 capitalize line-clamp-1'>
-                {name}
-              </h3>
-              <p className='col-start-2 col-end-3 row-start-2 row-end-3 tracking-wider text-sm'>
-                {`$${parseFloat(newPrice).toLocaleString('en-US', {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                })} x ${quantity}`}{' '}
-                <span className='font-bold text-blue-400'>
-                  $
-                  {parseFloat(newPrice * quantity).toLocaleString('en-US', {
+        <div className='p-4 lg:p-6 grid gap-4'>
+          {cart.map(({ id, name, newPrice, quantity, image }) => {
+            return (
+              <article
+                key={id}
+                className='grid grid-cols-[auto_1fr_auto] gap-x-4'
+              >
+                <div className='col-start-1 col-end-2 row-start-1 row-end-3 w-12'>
+                  <img className='rounded-md' src={image} alt={name} />
+                </div>
+                <h3 className='col-start-2 col-end-3 capitalize line-clamp-1'>
+                  {name}
+                </h3>
+                <p className='col-start-2 col-end-3 row-start-2 row-end-3 tracking-wider text-sm'>
+                  {`$${parseFloat(newPrice).toLocaleString('en-US', {
                     minimumFractionDigits: 2,
                     maximumFractionDigits: 2,
-                  })}
-                </span>
-              </p>
-              <button
-                onClick={() =>
-                  dispatchCart({ type: 'REMOVE_ITEM', payload: id })
-                }
-                type='button'
-                className='col-start-3 col-end-4 row-start-1 row-end-3 flex items-center'
-              >
-                <img src='./images/icon-delete.svg' alt='remove' />
-              </button>
-            </article>
-          );
-        })
+                  })} x ${quantity}`}{' '}
+                  <span className='font-bold text-blue-400'>
+                    $
+                    {parseFloat(newPrice * quantity).toLocaleString('en-US', {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
+                  </span>
+                </p>
+                <button
+                  onClick={() =>
+                    dispatchCart({ type: 'REMOVE_ITEM', payload: id })
+                  }
+                  type='button'
+                  className='col-start-3 col-end-4 row-start-1 row-end-3 flex items-center'
+                >
+                  <img src='./images/icon-delete.svg' alt='remove' />
+                </button>
+              </article>
+            );
+          })}
+          <button className='hover:opacity-90 transition-all block w-full font-bold text-blue-100 text-center bg-orange-200 rounded-md px-[1em] py-[.5em]'>
+            Checkout
+          </button>
+        </div>
       ) : (
         <h4 className='min-h-[8rem] grid place-items-center font-bold'>
           Your cart is empty.
